420be4190a591e1cede2831dced989780e00dc45
caicob <caicohenrique3@gmail.com>
2018-10-18 21:39:46 -0300

    Update public/pages/contato.php

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -8,23 +8,23 @@
 				<div class="input-group-prepend">
 					<span class="input-group-text" id="Name">Nome :</span>
 				</div>
-				<input type="text" class="form-control" placeholder="Digite seu nome! Ex:. Fulano da silva" aria-label="Name" aria-describedby="Name" name="name">
+				<input type="text" class="form-control" placeholder="Digite seu nome! Ex:. Fulano da silva" aria-label="Name" aria-describedby="Name" name="name" required>
 			</div>
 			<div class="input-group mb-1">
 				<div class="input-group-prepend">
 					<span class="input-group-text" id="email">E-mail :</span>
 				</div>
-				<input type="text" class="form-control" placeholder="Digite seu e-mail! Ex:. exemplo@exemplo.com" aria-label="email" aria-describedby="email" name="email">
+				<input type="email" class="form-control" placeholder="Digite seu e-mail! Ex:. exemplo@exemplo.com" aria-label="email" aria-describedby="email" name="email" required>
 			</div>
 			<div class="input-group mb-1">
 				<div class="input-group-prepend">
 					<span class="input-group-text" id="subject">Assunto :</span>
 				</div>
-				<input type="text" class="form-control" placeholder="Digite o assunto a ser tratado! Ex:. Dificuldades com botão x" aria-label="subject" aria-describedby="subject" name="subject">
+				<input type="text" class="form-control" placeholder="Digite o assunto a ser tratado! Ex:. Dificuldades com botão x" aria-label="subject" aria-describedby="subject" name="subject" required>
 			</div>
 			<div class="form-group">
 				<label for="message">Mensagem :</label>
-				<textarea class="form-control" id="message" rows="5" name="message"></textarea>
+				<textarea class="form-control" id="message" rows="5" name="message" required></textarea>
 			</div>
 		</div>
 		<div class="modal-footer">
