8671044b880bbcddd968c183f15016f19c7367dd
caicob <caicohenrique3@gmail.com>
2018-10-18 21:25:39 -0300

    colocando composer

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,4 @@
 
 session_start(); //inicia seção $_SESSION
 
-require "../app/functions/pages.php"; //chama funções das pages
-require "../app/functions/custom.php"; //chama funções gerais
-require "../app/functions/validate.php"; //chama funções gerais
+require "../vendor/autoload.php"; //puxar composer
